6ebde5a5358f1061320bfdbec243b18abb08f58b
codeGoCodeNot <johnsenberdin2930@gmail.com>
2026-04-28 09:39:39 +0800

    feat: add guard to get active org

--- src/features/organizations-feature/organization/queries/get-active-organization.ts
+++ src/features/organizations-feature/organization/queries/get-active-organization.ts
@@ -3,9 +3,22 @@ import prisma from "@/lib/prisma";
 
 const getActiveOrganization = async () => {
   const session = await getSession();
+  if (!session) return null;
+
   const activeOrganizationId = session?.session.activeOrganizationId;
 
-  if (!activeOrganizationId) return null;
+  if (!activeOrganizationId) {
+    const firstOrganization = await prisma.organization.findFirst({
+      where: {
+        members: {
+          some: {
+            userId: session.user.id,
+          },
+        },
+      },
+    });
+    return firstOrganization ?? null;
+  }
 
   return await prisma.organization.findUnique({
     where: { id: activeOrganizationId },
